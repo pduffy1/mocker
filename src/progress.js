3ed8d82616dd040483b7bc608df27f66a85ba0ae
Daniel Goldman <daniel.goldman@appian.com>
2023-08-02 14:22:54 -0400

    remove progress when done

--- src/progress.js
+++ src/progress.js
@@ -1,20 +1,9 @@
 function showProgress(progress, text = "") {
-  // Remember to print a newline once finished
-  const textWidth = 20;
-  const percentageWidth = 6;
-  const progressPlaceholder = progress
-    .toFixed(0)
-    .padStart(percentageWidth, " ");
-  const donePlaceholder = (
-    progress === 100 ? "\x1b[32mDone\x1b[0m" : ""
-  ).padEnd(4, " ");
-
   process.stdout.cursorTo(0);
   process.stdout.write(
-    `${text.padEnd(
-      textWidth,
-      " "
-    )} ${progressPlaceholder}%    ${donePlaceholder}`
+    `${text}... ${
+      progress === 100 ? "\x1b[32mDone\x1b[0m" : `${progress.toFixed(0)}%`
+    }`
   );
 }
 
